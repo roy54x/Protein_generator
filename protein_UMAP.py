b7991e9fd785e6c933cc7cd7203fa2b5949d078e
Roy.ilani <Roy.ilani@vayyar.com>
2024-03-26 16:30:17 +0200

    plot by organism colors

--- protein_UMAP.py
+++ protein_UMAP.py
@@ -14,17 +14,33 @@ def encode_sequences(sequences):
     return encoded_sequences
 
 
-def pad_vectors(vectors):
-    max_length = max(len(v) for v in vectors)
-    padded_vectors = [v + [0.0] * (max_length - len(v)) for v in vectors]
-    return padded_vectors
+def pad_sequences(sequences):
+    max_length = max(len(s) for s in sequences)
+    padded_sequences = [np.array(s + [0.0] * (max_length - len(s))) for s in sequences]
+    return padded_sequences
+
+
+def encode_values(value_list):
+    unique_values = sorted(set(value_list))
+    value_to_numeric = {value: i for i, value in enumerate(unique_values)}
+    encoded_values = [value_to_numeric[value] for value in value_list]
+    return encoded_values, value_to_numeric
 
 
 protein_df = pd.read_csv("protein_df.csv")
+top_5_organisms = protein_df['organism'].value_counts().head(5).index.tolist()
+protein_df = protein_df[protein_df['organism'].isin(top_5_organisms)]
 encoded_sequences = encode_sequences(protein_df.sequence)
-padded_vectors = pad_vectors(encoded_sequences)
-umap_colors = protein_df.organism
+padded_sequences = pad_sequences(encoded_sequences)
+
+encoded_values, value_to_numeric = encode_values(protein_df.organism.to_list())
 reducer = umap.UMAP(n_neighbors=15)
-embedding = reducer.fit_transform(padded_vectors)
+embedding = reducer.fit_transform(padded_sequences)
 fig, ax = plt.subplots()
-sc = ax.scatter(embedding[:, 0], embedding[:, 1], c=umap_colors, cmap='Spectral', lw=0, alpha=1, s=5)
+sc = ax.scatter(embedding[:, 0], embedding[:, 1], c=encoded_values, cmap='Spectral', lw=0, alpha=1, s=5)
+
+numeric_to_value = {numeric: value for value, numeric in value_to_numeric.items()}
+legend_labels = [numeric_to_value[numeric] for numeric in sorted(value_to_numeric.values())]
+legend = ax.legend(legend_labels, title='Organism', loc='upper right', fontsize='small')
+plt.setp(legend.get_title(), fontsize='medium')
+plt.show()

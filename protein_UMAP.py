4450a2f2af4e5624bca2794aa791ba1f801da4f0
Roy.ilani <Roy.ilani@vayyar.com>
2024-03-26 15:18:36 +0200

    functions splited

--- protein_UMAP.py
+++ protein_UMAP.py
@@ -1,48 +1,7 @@
 import numpy as np
 import pandas as pd
 import umap.umap_ as umap
-from lxml import etree
 from matplotlib import pyplot as plt
-
-
-def parse_uniprot_xml(xml_file):
-    data = []
-    context = etree.iterparse(xml_file, events=('end',), tag='{http://uniprot.org/uniprot}entry')
-    for event, elem in context:
-        accession = elem.findtext('{http://uniprot.org/uniprot}accession')
-        sequence = elem.findtext('{http://uniprot.org/uniprot}sequence')
-        protein_names = [name.text for name in elem.findall('.//{http://uniprot.org/uniprot}fullName')]
-        organism = elem.findtext('.//{http://uniprot.org/uniprot}organism/{http://uniprot.org/uniprot}name')
-        function = elem.findtext('.//{http://uniprot.org/uniprot}comment[@type="function"]/{http://uniprot.org/uniprot}text')
-        subcellular_location = elem.findtext('.//{http://uniprot.org/uniprot}comment[@type="subcellular location"]/{http://uniprot.org/uniprot}subcellularLocation/{http://uniprot.org/uniprot}location')
-        tissue_specificity = elem.findtext('.//{http://uniprot.org/uniprot}comment[@type="tissue specificity"]/{http://uniprot.org/uniprot}text')
-        domain_structure = [domain.get('description') for domain in
-                            elem.findall('.//{http://uniprot.org/uniprot}feature[@type="domain"]')]
-        ptms = [ptm.get('description') for ptm in
-                elem.findall('.//{http://uniprot.org/uniprot}feature[@type="modified residue"]')]
-        interactions = [interaction.text for interaction in
-                        elem.findall('.//{http://uniprot.org/uniprot}interactant/{http://uniprot.org/uniprot}geneName')]
-        sequence_annotations = [(annot.get('description'), annot.get('evidence')) for annot in
-                                elem.findall('.//{http://uniprot.org/uniprot}feature')]
-
-        data.append({
-            'accession': accession,
-            'sequence': sequence,
-            'protein_names': protein_names,
-            'organism': organism,
-            'function': function,
-            'subcellular_location': subcellular_location,
-            'tissue_specificity': tissue_specificity,
-            'domain_structure': domain_structure,
-            'ptms': ptms,
-            'interactions': interactions,
-            'sequence_annotations': sequence_annotations,
-        })
-        elem.clear()
-        while elem.getprevious() is not None:
-            del elem.getparent()[0]
-
-    return pd.DataFrame(data)
 
 
 def one_hot_encode_sequence(sequence):
@@ -74,11 +33,10 @@ def tokenize_sequences_one_hot(sequences):
     return tokens
 
 
-xml_file_path = r'C:\Users\RoyIlani\Downloads\uniprot_sprot.xml'
-protein_df = parse_uniprot_xml(xml_file_path)
+protein_df = pd.read_csv("protein_df.csv")
 sequences = protein_df.sequence
 tokens = tokenize_sequences_one_hot(sequences)
-umap_colors = None
+umap_colors = protein_df.organism
 reducer = umap.UMAP(n_neighbors=15)
 embedding = reducer.fit_transform(tokens)
 fig, ax = plt.subplots()

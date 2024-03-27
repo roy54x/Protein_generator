edf10acbd8407e23e57ec7aa609209840b32e865
Roy.ilani <Roy.ilani@vayyar.com>
2024-03-27 10:59:37 +0200

    back to one hot

--- protein_UMAP.py
+++ protein_UMAP.py
@@ -4,20 +4,22 @@ import umap.umap_ as umap
 from matplotlib import pyplot as plt
 
 
-def encode_sequences(sequences):
+def one_hot_encode_sequence(sequences):
     amino_acids = 'ACDEFGHIKLMNPQRSTVWY'
-    encoding = {aa: i / len(amino_acids) for i, aa in enumerate(amino_acids)}
+    aa_to_index = {aa: i for i, aa in enumerate(amino_acids)}
+    num_amino_acids = len(amino_acids)
+    max_len = max([len(s) for s in sequences])
+
     encoded_sequences = []
-    for sequence in sequences:
-        encoded_sequence = [encoding.get(aa, 0.0) for aa in sequence]
-        encoded_sequences.append(encoded_sequence)
+    for s in sequences:
+        encoding = np.zeros((max_len, num_amino_acids))
+        for i, aa in enumerate(s):
+            if aa in aa_to_index:
+                encoding[i, aa_to_index[aa]] = 1
+            else:
+                encoding[i, :] = np.nan
+        encoded_sequences.append(encoding)
     return encoded_sequences
-
-
-def pad_sequences(sequences):
-    max_length = max(len(s) for s in sequences)
-    padded_sequences = [np.array(s + [-1.0] * (max_length - len(s))) for s in sequences]
-    return padded_sequences
 
 
 def encode_values(value_list):
@@ -29,14 +31,13 @@ def encode_values(value_list):
 
 protein_df = pd.read_csv("protein_df.csv")
 top_5_organisms = protein_df['organism'].value_counts().head(5).index.tolist()
-protein_df = protein_df[protein_df['organism'].isin(top_5_organisms)][:2000]
-encoded_sequences = encode_sequences(protein_df.sequence)
-padded_sequences = pad_sequences(encoded_sequences)
+protein_df = protein_df[protein_df['organism'].isin(top_5_organisms)][:500]
+encoded_sequences = one_hot_encode_sequence(protein_df.sequence)
 encoded_values, value_to_numeric = encode_values(protein_df.organism.to_list())
 numeric_to_value = {numeric: value for value, numeric in value_to_numeric.items()}
 
 reducer = umap.UMAP()
-embedding = reducer.fit_transform(padded_sequences)
+embedding = reducer.fit_transform(encoded_sequences)
 fig, ax = plt.subplots()
 
 for numeric, value in numeric_to_value.items():

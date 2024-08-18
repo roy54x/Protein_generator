17fceb49c1939b5be728fb3a7178d2b830baa8f3
RoyIlani-neuray <roy.ilani@neuray.ai>
2024-08-18 19:46:57 +0300

    trivial

--- strategies/sequence_to_binding_sequence.py
+++ strategies/sequence_to_binding_sequence.py
@@ -10,7 +10,7 @@ from utils.padding_functions import padd_sequence
 
 
 class DiffusionModule(nn.Module):
-    def __init__(self, hidden_size, timesteps=10, num_layers=3, noise_steps=5):
+    def __init__(self, hidden_size, timesteps=100, num_layers=3, noise_steps=5):
         super(DiffusionModule, self).__init__()
         self.timesteps = timesteps
         self.hidden_size = hidden_size
@@ -36,18 +36,19 @@ class SequenceDiffusionModel(Base):
         self.vocab_size = len(AMINO_ACIDS) + 1
         self.min_split_percent, self.max_split_percent = 0.5, 0.9
         self.max_input_size, self.max_output_size = int(MAX_SIZE * self.max_split_percent), int(MAX_SIZE * self.min_split_percent)
+        self.hidden_size = 18
 
         config = transformers.RobertaConfig(
             vocab_size=self.vocab_size,
-            max_position_embeddings=252,
-            hidden_size=36,
+            max_position_embeddings=self.max_input_size+2,
+            hidden_size=self.hidden_size,
             num_attention_heads=6,
             num_hidden_layers=6,
             type_vocab_size=1
         )
         self.transformer = transformers.RobertaModel(config=config)
-        self.diffusion = DiffusionModule(hidden_size=36, timesteps=100, num_layers=3)
-        self.output_layer = nn.Linear(36, self.max_output_size
+        self.diffusion = DiffusionModule(hidden_size=self.hidden_size, timesteps=100, num_layers=3)
+        self.output_layer = nn.Linear(self.hidden_size, self.max_output_size
                                       * self.vocab_size)
 
     def load_inputs_and_ground_truth(self, data):

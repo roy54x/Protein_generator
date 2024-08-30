f421c2e47ac2114cfb089e669fe862d73f3beae2
RoyIlani-neuray <roy.ilani@neuray.ai>
2024-08-31 00:25:49 +0300

    bug fix

--- strategies/sequence_to_distogram.py
+++ strategies/sequence_to_distogram.py
@@ -43,7 +43,7 @@ class SequenceToDistogram(Base):
             start, end = self.get_augmentation_indices(len(sequence))
             sequence = sequence[start: end]
         else:
-            sequence = sequence[:MAX_TRAINING_SIZE, :MAX_TRAINING_SIZE]
+            sequence = sequence[:MAX_TRAINING_SIZE]
         x_tensor, mask_tensor = padd_sequence(sequence, MAX_TRAINING_SIZE)
 
         # Get ground truth
@@ -52,7 +52,8 @@ class SequenceToDistogram(Base):
             distogram = distogram[start: end, start: end]
         else:
             distogram = distogram[:MAX_TRAINING_SIZE, :MAX_TRAINING_SIZE]
-        distogram = np.where(np.ptp(distogram) > 0, (distogram - np.min(distogram)) / np.ptp(distogram), 0)
+        distogram = np.where(np.ptp(distogram) > 0,
+                             (distogram - np.min(distogram)) / np.ptp(distogram), 0)
         ground_truth = padd_contact_map(distogram, MAX_TRAINING_SIZE)
 
         return (x_tensor, mask_tensor), ground_truth

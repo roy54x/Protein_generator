18234d82c4b608478fb4ed4af8ab49e79e7fb7d7
RoyIlani-neuray <roy.ilani@neuray.ai>
2024-12-20 15:01:02 +0200

    coords to sequence done

--- strategies/coords_to_sequence.py
+++ strategies/coords_to_sequence.py
@@ -6,48 +6,57 @@ from esm.inverse_folding.util import CoordBatchConverter
 
 from constants import MAX_TRAINING_SIZE, AMINO_ACIDS, MIN_SIZE
 from strategies.base import Base
+from utils.padding_functions import padd_sequence
 
 
 class CoordsToSequence(Base):
 
     def __init__(self):
         super(CoordsToSequence, self).__init__()
-        model, self.alphabet = esm.pretrained.esm_if1_gvp4_t16_142M_UR50()
-        self.args = model.args
-        self.args.encoder_embed_dim = 128
-        self.args.decoder_embed_dim = 128
-        self.args.decoder_input_dim = 128
-        self.args.decoder_output_dim = 128
-        self.args.encoder_ffn_embed_dim = 512
-        self.args.decoder_ffn_embed_dim = 512
+        self.model, self.alphabet = esm.pretrained.esm_if1_gvp4_t16_142M_UR50()
+
+        self.args = self.model.args
+        self.args.encoder_embed_dim = 64
+        self.args.decoder_embed_dim = 64
+        self.args.decoder_input_dim = 64
+        self.args.decoder_output_dim = 64
+        self.args.encoder_ffn_embed_dim = 256
+        self.args.decoder_ffn_embed_dim = 256
         self.args.encoder_layers = 4
         self.args.decoder_layers = 4
         self.args.encoder_attention_heads = 4
         self.args.decoder_attention_heads = 4
-        self.args.gvp_node_hidden_dim_scalar = 256
-        self.args.gvp_node_hidden_dim_vector = 64
-        self.args.gvp_edge_hidden_dim_scalar = 8
+        self.args.gvp_node_hidden_dim_scalar = 128
+        self.args.gvp_node_hidden_dim_vector = 32
+        self.args.gvp_edge_hidden_dim_scalar = 4
+        self.args.max_tokens = MAX_TRAINING_SIZE
 
         self.gvp_transformer = GVPTransformerModel(self.args, self.alphabet)
         self.batch_converter = CoordBatchConverter(self.alphabet)
-        self.device = next(model.parameters()).device
+        self.device = next(self.model.parameters()).device
 
-    def load_inputs_and_ground_truth(self, data, end=None):
-        sequence = data['sequence']
-        if self.training:
-            start, end = self.get_augmentation_indices(len(sequence))
-        elif end:
-            start, end = max(0, end - MAX_TRAINING_SIZE), end
-        else:
-            start, end = 0, MAX_TRAINING_SIZE
-        sequence = sequence[start: end]
-        coords = data["coords"][start: end]
+    def load_inputs_and_ground_truth(self, batch_data, end=None):
+        batch_converter_input = []
 
-        return (coords, None, sequence), None
+        for data in batch_data:
+            sequence = data['sequence']
+            coords = data['coords']
 
-    def collate(self, batch):
-        batch = [x[0] for x in batch]
-        coords, confidence, strs, tokens, padding_mask = self.batch_converter(batch, device=self.device)
+            if self.training:
+                start, end = self.get_augmentation_indices(len(sequence))
+            elif end:
+                start, end = max(0, end - MAX_TRAINING_SIZE), end
+            else:
+                start, end = 0, MAX_TRAINING_SIZE
+
+            sequence = sequence[start:end]
+            coords = coords[start: end]
+
+            batch_converter_input.append((coords, None, sequence))
+
+        coords, confidence, strs, tokens, padding_mask = self.batch_converter(
+            batch_converter_input, device=self.device)
+
         prev_output_tokens = tokens[:, :-1]
         ground_truth = tokens[:, 1:]
 
